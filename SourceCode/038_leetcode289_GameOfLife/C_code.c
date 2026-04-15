87b81b84893c4c3a35893896696ce10b7cb54045
Code Under Hood <codeunderhood@gmail.com>
2026-04-15 19:53:58 +0530

    Update C_code.c

--- SourceCode/038_leetcode289_GameOfLife/C_code.c
+++ SourceCode/038_leetcode289_GameOfLife/C_code.c
@@ -11,6 +11,7 @@ When counting neighbors, treat:
 1 and 2 as originally live
 0 and 3 as originally dead
 */
+#include <stdio.h>
 #include <stdlib.h>
 
 int directions[8][2] = {
@@ -27,7 +28,6 @@ int countLiveNeighbors(int** board, int m, int n, int row, int col) {
         int newCol = col + directions[d][1];
 
         if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n) {
-            // original live cells: 1 and 2
             if (board[newRow][newCol] == 1 || board[newRow][newCol] == 2) {
                 count++;
             }
@@ -66,4 +66,49 @@ void gameOfLife(int** board, int boardSize, int* boardColSize) {
             else if (board[row][col] == 3) board[row][col] = 1;
         }
     }
+}
+
+int main() {
+    int m = 3, n = 3;
+
+    // Allocate board
+    int** board = (int**)malloc(m * sizeof(int*));
+    for (int i = 0; i < m; i++) {
+        board[i] = (int*)malloc(n * sizeof(int));
+    }
+
+    // Initialize board
+    int temp[3][3] = {
+        {0, 1, 0},
+        {0, 1, 0},
+        {0, 1, 0}
+    };
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            board[i][j] = temp[i][j];
+        }
+    }
+
+    int boardColSize[3] = {n, n, n};
+
+    // Call function
+    gameOfLife(board, m, boardColSize);
+
+    // Print result
+    printf("Updated Board:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", board[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Free memory
+    for (int i = 0; i < m; i++) {
+        free(board[i]);
+    }
+    free(board);
+
+    return 0;
 }
