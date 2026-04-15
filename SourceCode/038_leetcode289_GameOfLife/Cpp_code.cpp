cf7a0a28f2588ebc00027077b4a49291fe6fa4f4
Code Under Hood <codeunderhood@gmail.com>
2026-04-15 20:02:39 +0530

    Create Cpp_code.cpp

---
+++ SourceCode/038_leetcode289_GameOfLife/Cpp_code.cpp
@@ -0,0 +1,60 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> directions = {
+        {-1, -1}, {-1, 0}, {-1, 1},
+        {0, -1},           {0, 1},
+        {1, -1},  {1, 0},  {1, 1}
+    };
+
+    int countLiveNeighbors(vector<vector<int>>& board, int m, int n, int row, int col) {
+        int count = 0;
+
+        for (int d = 0; d < 8; d++) {
+            int newRow = row + directions[d][0];
+            int newCol = col + directions[d][1];
+
+            if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n) {
+                // original live cells: 1 and 2
+                if (board[newRow][newCol] == 1 || board[newRow][newCol] == 2) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    void gameOfLife(vector<vector<int>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+
+        // First pass: apply transitions
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+
+                int liveNeighbors = countLiveNeighbors(board, m, n, row, col);
+
+                if (board[row][col] == 1) {
+                    if (liveNeighbors < 2 || liveNeighbors > 3) {
+                        board[row][col] = 2; // live -> dead
+                    }
+                } else {
+                    if (liveNeighbors == 3) {
+                        board[row][col] = 3; // dead -> live
+                    }
+                }
+            }
+        }
+
+        // Second pass: finalize states
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (board[row][col] == 2) board[row][col] = 0;
+                else if (board[row][col] == 3) board[row][col] = 1;
+            }
+        }
+    }
+};
